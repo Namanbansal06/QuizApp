00e10b574e3b54c18cef5ae08fa3485888843685
Naman Bansal <bansalnaman44@gmail.com>
2023-01-10 00:03:04 +0530

    Quiz app

---
+++ app/src/main/java/com/example/navigation/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.navigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.navigation.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        @Suppress("UNUSED_VARIABLE")
+        val binding =
+            DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
+}
